b2b4fae531890fec35baae10517afb36aca29a70
Dylan Yeo <dylanyeowf@gmail.com>
2026-09-07 22:42:22 +0800

    feat: added caching to static files

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { visualizer } from 'rollup-plugin-visualizer';
 import preact from "@preact/preset-vite";
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +12,26 @@ export default defineConfig({
 			open: true,
 			gzipSize: true,
 			brotliSize: true,
-		})
+		}),
+		VitePWA({
+			registerType: "autoUpdate",
+			workbox: {
+				runtimeCaching: [{
+					urlPattern: /^https:\/\/dqmldihyiupobespwasu\.supabase\.co\/storage\/v1\/object\/public\//,
+					handler: "StaleWhileRevalidate",
+					options: {
+						cacheName: "supabase-images",
+						cacheableResponse: {
+							statuses: [0, 200], 
+						},
+						expiration: {
+							maxEntries: 100,
+							maxAgeSeconds: 60 * 60 * 24 * 7,
+						},
+					},
+				}]
+			}
+		}),
 	],
 	resolve: {
 		tsconfigPaths: true
